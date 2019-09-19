93bcbbe7a42683c3e6feaf07f87a0cf744db5a32
Manuel Montenegro <manumonti@gmail.com>
2019-09-19 19:35:44 +0200

    Now simulation is played in loop

--- modules/in_ObdGpsBeaconsTrip.py
+++ modules/in_ObdGpsBeaconsTrip.py
@@ -43,82 +43,86 @@ class ObdGpsBeaconsTrip:
     # read simulated session DB and parse it into data units
     def dbReader ( self ):
 
-        # connection to database
-        db=sqlite3.connect(self.sessionPath)
-        gpsCurs = db.cursor()
-        obdCurs = db.cursor()
-        if self.beaconsTripEnabled:
-            beaconsDataCurs = db.cursor()
+        while not self.threadStopEvent.is_set():
+
+            # connection to database
+            db=sqlite3.connect(self.sessionPath)
+
+            gpsCurs = db.cursor()
+            obdCurs = db.cursor()
+            
+            if self.beaconsTripEnabled:
+                beaconsDataCurs = db.cursor()
         
-        try:
-            gpsCurs.execute("SELECT * FROM gps")
-            obdCurs.execute("SELECT * FROM obd")
-            if self.beaconsTripEnabled:
-                beaconsDataCurs.execute("SELECT * FROM beacons_data")
+            try:
+                gpsCurs.execute("SELECT * FROM gps")
+                obdCurs.execute("SELECT * FROM obd")
+                if self.beaconsTripEnabled:
+                    beaconsDataCurs.execute("SELECT * FROM beacons_data")
 
-            lastTime = None
-            sleepNextTime = None
-            sleepCurrentTime = None
+                lastTime = None
+                sleepNextTime = None
+                sleepCurrentTime = None
 
-            if self.beaconsTripEnabled:
-                beacons_dataRow = beaconsDataCurs.fetchone()
+                if self.beaconsTripEnabled:
+                    beacons_dataRow = beaconsDataCurs.fetchone()
 
-            while not self.threadStopEvent.is_set():
-                # collect first row data from gps table
-                gpsRow = gpsCurs.fetchone()
-                # collect first row data from obd table
-                obdRow = gpsCurs.fetchone()
+                while not self.threadStopEvent.is_set():
+                    # collect first row data from gps table
+                    gpsRow = gpsCurs.fetchone()
+                    # collect first row data from obd table
+                    obdRow = gpsCurs.fetchone()
 
-                # if the end of database table is reached, exit from loop
-                if gpsRow == None or obdRow == None:
-                    break
+                    # if the end of database table is reached, exit from loop
+                    if gpsRow == None or obdRow == None:
+                        break
 
-                # simulation of time taken between each sample from database
-                if sleepNextTime != None:
-                    sleepCurrentTime = sleepNextTime
-                    sleepNextTime = gpsRow [6]
-                    sleepDiff = sleepNextTime - sleepCurrentTime
-                    time.sleep(sleepDiff)
-                else:
-                    sleepNextTime = gpsRow[6]
+                    # simulation of time taken between each sample from database
+                    if sleepNextTime != None:
+                        sleepCurrentTime = sleepNextTime
+                        sleepNextTime = gpsRow [6]
+                        sleepDiff = sleepNextTime - sleepCurrentTime
+                        time.sleep(sleepDiff)
+                    else:
+                        sleepNextTime = gpsRow[6]
 
-                # get data values from database
-                lat = gpsRow[0]
-                lon = gpsRow[1]
-                course = int(gpsRow[4])
-                gpsTime = int(gpsRow[5])
-                vss = int(obdRow[2])
+                    # get data values from database
+                    lat = gpsRow[0]
+                    lon = gpsRow[1]
+                    course = int(gpsRow[4])
+                    gpsTime = int(gpsRow[5])
+                    vss = int(obdRow[2])
 
-                # filter samples by read step: discard samples too close
-                if lastTime is None or (lastTime+self.readStep) <= gpsRow[6]:
-                    lastTime = gpsRow[6]
-                    # structure for generated JSON
-                    carData = {"carInfo": {"engineOn":True, "vss":vss, "lat":lat, "lon":lon, "gpsTime":gpsTime, "course":course}}
+                    # filter samples by read step: discard samples too close
+                    if lastTime is None or (lastTime+self.readStep) <= gpsRow[6]:
+                        lastTime = gpsRow[6]
+                        # structure for generated JSON
+                        carData = {"carInfo": {"engineOn":True, "vss":vss, "lat":lat, "lon":lon, "gpsTime":gpsTime, "course":course}}
 
-                    # put on the queue the data collected
-                    self.dataQueue.put(carData)
+                        # put on the queue the data collected
+                        self.dataQueue.put(carData)
 
-                    if self.beaconsTripEnabled:
-                        # if there is beacon data for this instant of time, add to data 
-                        if beacons_dataRow != None and beacons_dataRow[0] <= lastTime:
-                            # load JSON data from beacons_data database table
-                            sensorData = json.loads (beacons_dataRow[1])
+                        if self.beaconsTripEnabled:
+                            # if there is beacon data for this instant of time, add to data 
+                            if beacons_dataRow != None and beacons_dataRow[0] <= lastTime:
+                                # load JSON data from beacons_data database table
+                                sensorData = json.loads (beacons_dataRow[1])
 
-                            # put on the queue the sensor data collected
-                            self.dataQueue.put(sensorData)
+                                # put on the queue the sensor data collected
+                                self.dataQueue.put(sensorData)
 
-                            # take the following row
-                            beacons_dataRow = beaconsDataCurs.fetchone()
+                                # take the following row
+                                beacons_dataRow = beaconsDataCurs.fetchone()
 
-        except sqlite3.DatabaseError:
-            print ('Database Error\r\n')
+            except sqlite3.DatabaseError:
+                print ('Database Error\r\n')
 
-        except queue.Full:
-            print('The queue is full\r\n')
+            except queue.Full:
+                print('The queue is full\r\n')
 
-        except Exception as error:
-            print ("Unknown error\r\n ", error)
+            except Exception as error:
+                print ("Unknown error\r\n ", error)
 
-        finally:
-            # close safely the database
-            db.close()
+            finally:
+                # close safely the database
+                db.close()
